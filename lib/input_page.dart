6534da5a6c583b73cf570dee97016cb264f1f400
AjayRaturi <ajay.raturi22@gmail.com>
2022-01-31 23:37:48 +0530

    test

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-const bottomContainerHeight =80.0;
-const activeCardColour =0xFF1D1E33;
-const bottomContainerColour= Color(0xFFEB1555);
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = 0xFF1D1E33;
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -22,6 +25,25 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: ReUsableCard(
                   colour: Color(activeCardColour),
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      )
+                    ],
+                  ),
                 )),
                 Expanded(
                     child: ReUsableCard(
@@ -58,11 +80,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReUsableCard extends StatelessWidget {
-  ReUsableCard({@required this.colour});
+  ReUsableCard({@required this.colour, this.cardChild});
   final Color colour;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),

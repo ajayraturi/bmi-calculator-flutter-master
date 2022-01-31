1fc15e20f85bf168941d840383af2fe1e612d31c
AjayRaturi <ajay.raturi22@gmail.com>
2022-01-31 23:24:08 +0530

    test

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+const bottomContainerHeight =80.0;
+const activeCardColour =0xFF1D1E33;
+const bottomContainerColour= Color(0xFFEB1555);
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,31 +21,37 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                     child: ReUsableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: Color(activeCardColour),
                 )),
                 Expanded(
                     child: ReUsableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: Color(activeCardColour),
                 )),
               ],
             )),
             Expanded(
                 child: ReUsableCard(
-              colour: Color(0xFF1D1E33),
+              colour: Color(activeCardColour),
             )),
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
                     child: ReUsableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: Color(activeCardColour),
                 )),
                 Expanded(
                     child: ReUsableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: Color(activeCardColour),
                 )),
               ],
             )),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
